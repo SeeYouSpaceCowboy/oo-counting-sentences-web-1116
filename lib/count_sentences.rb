00169f87029593b92621e3838cc6914bf604283d
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-02 16:01:54 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1] == "." ? true : false
   end
 
   def question?
-
+      self[self.length-1] == "?" ? true : false
   end
 
   def exclamation?
-
+      self[self.length-1] == "!" ? true : false
   end
 
   def count_sentences
+    counter = 0
 
+    self.split(" ").each do |sentence|
+      if sentence.sentence? || sentence.question? || sentence.exclamation?
+        counter += 1
+      end
+    end
+
+    counter
   end
 end
